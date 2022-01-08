af67aa237d4477906fdca979895fc36867d0a448
crof2003 <20980923+crof2003@users.noreply.github.com>
2022-01-08 13:37:24 -0500

    Squashed commit of the following:
    commit 5473fe3bd103f21a750fddd9caaf78c13abc22e1
    Author: crof2003 <20980923+crof2003@users.noreply.github.com>
    Date:   Sat Jan 8 13:36:44 2022 -0500
    
        display bug fix
    
    commit 9dedbb51a61da8aac5ad25713e5336a1431617a6
    Author: crof2003 <20980923+crof2003@users.noreply.github.com>
    Date:   Mon Jan 3 23:40:04 2022 -0500
    
        Working Start and Pause Buttons

--- WorkdayTimer.ino
+++ WorkdayTimer.ino
@@ -19,7 +19,7 @@
 #include <TFT_eSPI.h>       // Hardware-specific library
 
 /******Definitions*****/
-#define PROGRAM_VERSION "0.0.1"
+
 
 #define TOP_BUTTON_PIN  35 //Top/Right button
 #define BOTTOM_BUTTON_PIN 0 //Bottom/Left button
@@ -34,24 +34,35 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
 
 /******variable definitions*****/
 float percent_complete;
-int total_time_hours = 8;
+int total_time_hours = 7;
 int total_time_minutes = 30;
 int total_time_seconds = 0;
-int total_timed_ms = ((((total_time_hours*60)+total_time_minutes)*60)+total_time_seconds)*1000;
+int workday_ms_total = ((((total_time_hours*60)+total_time_minutes)*60)+total_time_seconds)*1000;
 int start_ms;
 
 long total_working_ms = 0;
+long current_working_ms_start = 0;
+long total_break_ms = 0;
+long current_break_ms_start = 0;
 long total_paid_break_bank_ms = (30*60*1000); //30 minutes paid break per 8 hours
 long current_times_ms_start = 0;
 
-int display_update_interval_ms = 500;
+int display_update_interval_ms = (1000/30);
 long last_diplay_update_ms = 0;
+
+long last_full_diplay_update_ms = 0;
+long full_dispaly_update_interval_ms = 10000;
+
 
 boolean default_display = true;
 boolean currently_working = false;
 boolean currently_on_break = false;
 
+String program_version = "0.0.1";
+String default_display_banner_text = "Work Timer - v" + default_display_banner_text;
+
 void debug_display_text(String in_str, int in_x=0, int in_y=80);
+String ms_to_time(long in_ms, boolean include_ms=false);
 
 /*******Setup******/
 void setup() {
@@ -59,6 +70,12 @@ void setup() {
     // Screen setup....
     tft.init();
     tft.setRotation(1); // Rotate the screen to landscape
+
+    // Backlight Setup
+    pinMode(TFT_BL, OUTPUT);
+    ledcSetup(0, 5000, 8); // 0-15, 5000, 8
+    ledcAttachPin(TFT_BL, 0); // TFT_BL, 0 - 15
+    adjust_backlight(255);
 
     tft.fillScreen(TFT_BLACK);
 
@@ -71,16 +88,16 @@ void setup() {
 
   // Print some text
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.print("Runtime counter\n");
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.println("Work Timer");
+  tft.print(default_display_banner_text);
+  //tft.setTextColor(TFT_RED, TFT_BLACK);
+  //tft.println("Work Timer");
     tft.setTextSize(1);
 
   Serial.begin(115200);
   delay(50);
 
   Serial.print("\n\nWorkday Timer \nVersion: ");
-  Serial.print(PROGRAM_VERSION);
+  Serial.print(default_display_banner_text);
 
   top_button.begin(TOP_BUTTON_PIN);
   bottom_button.begin(BOTTOM_BUTTON_PIN);
@@ -117,10 +134,15 @@ void loop() {
 
   //Passivly update screen every so often
   if (millis() >= (last_diplay_update_ms+display_update_interval_ms)){
+    if(full_dispaly_update_interval_ms>(millis()-last_full_diplay_update_ms)){
+        update_default_display(true);
+        last_full_diplay_update_ms = millis();
+    }else
+        update_default_display(false);
     last_diplay_update_ms = millis();
-    debug_display_text(String(millis()),0,100);
-    debug_display_text(String((last_diplay_update_ms+display_update_interval_ms)),0,60);
-  }/*else{
+  }
+
+  /*}else{
     debug_display_text("---"+String(millis()),0,100);
     debug_display_text(String((last_diplay_update_ms+display_update_interval_ms)),0,60);
   }*/
@@ -138,26 +160,179 @@ void ttap(Button2& btn) {
     Serial.println(btn.getAttachPin());
     debug_display_text("ttap");
 
-    pinMode(TFT_BL, OUTPUT);
-ledcSetup(0, 5000, 8); // 0-15, 5000, 8
-ledcAttachPin(TFT_BL, 0); // TFT_BL, 0 - 15
-ledcWrite(0, 255); // 0-15, 0-255 (with 8 bit resolution); 0=totally dark;255=totally shiny
+    adjust_backlight(40);
+
+    if(currently_on_break){
+        end_timing_break();
+    }
+    if(!currently_working){
+        start_timing_work();
+    }
+
+    //Update the full screen...
+    update_default_display(true);
 }
 void btap(Button2& btn) {
     Serial.println("btap");
     Serial.println(btn.getAttachPin());
     debug_display_text("btap");
+    adjust_backlight(255);
 
-    pinMode(TFT_BL, OUTPUT);
-ledcSetup(0, 5000, 8); // 0-15, 5000, 8
-ledcAttachPin(TFT_BL, 0); // TFT_BL, 0 - 15
-ledcWrite(0, 1); // 0-15, 0-255 (with 8 bit resolution); 0=totally dark;255=totally shiny
+    if(currently_working){
+        end_timing_work();
+    }
+    if(!currently_on_break){
+        start_timing_break();
+    }
+
+    //Update the full screen...
+    update_default_display(true);
+
+    
 }
 
 void debug_display_text(String in_str, int in_x, int in_y){
     tft.setCursor(in_x, in_y, 4);
     tft.setTextSize(1);
     tft.println(in_str);
+}
+
+void start_timing_work(){
+    if(!currently_working){
+        current_working_ms_start = millis();
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        default_display_banner_text = "Working      ";
+        currently_working = true;
+    }
+}
+
+void end_timing_work(){
+    if(currently_working){
+        total_working_ms = total_working_ms + (current_break_ms_start + millis());
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        default_display_banner_text = "    Work End      ";
+        currently_working = false;
+    }
+}
+
+void start_timing_break(){
+    if(!currently_on_break){
+        current_break_ms_start = millis();
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        default_display_banner_text = "On Break...";
+        currently_on_break = true;
+    }
+}
+
+void end_timing_break(){
+    if(currently_on_break){
+        total_break_ms = total_break_ms + (millis() - current_break_ms_start);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        default_display_banner_text = "    Break End      ";
+        currently_on_break = false;
+    }
+}
+
+////////////
+// Updates the display that shows how much time is remaining
+///////////
+void update_default_display(boolean update_full_screen){
+    if(update_full_screen){
+        if(workday_ms_total<=get_total_working_ms()){
+            tft.fillScreen(TFT_WHITE);
+            adjust_backlight(255);
+            display_update_interval_ms = 1000;
+        }
+    
+    //Display the banner...
+    tft.setCursor(0, 0, 4);
+    if(currently_working)
+        tft.setTextColor(TFT_BLUE, TFT_BLACK);
+    else
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.print(default_display_banner_text);
+    }
+
+    //Display Total working time...
+    if(update_full_screen || currently_working){
+        tft.setCursor(0, 50, 4);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.print(ms_to_time(get_total_working_ms(),false));
+
+        tft.print(" | ");
+        tft.print(ms_to_time(workday_ms_total-get_total_working_ms()));
+    }
+    
+
+    //Display Total break time...
+    if(update_full_screen || currently_on_break){
+        tft.setCursor(0, 80, 4);
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.print(ms_to_time(get_total_break_ms(),false));
+    }
+
+    //debug_display_text(ms_to_time(get_total_break_ms()));
+}
+
+void adjust_backlight(int intensity){
+    ledcWrite(0, intensity); // 0-15, 0-255 (with 8 bit resolution); 0=totally dark;255=totally shiny
+}
+
+////////////
+// Returns the current total ms breaks taken - taking into account current working ms totals
+///////////
+long get_total_break_ms(){
+    if(currently_on_break){
+        return (total_break_ms + (millis() - current_break_ms_start));
+    }else{
+        return total_break_ms;
+    }
+}
+
+////////////
+// Returns the current total ms worked - taking into account current working ms totals
+///////////
+long get_total_working_ms(){
+    if(currently_working){
+        return total_working_ms + (millis() - current_working_ms_start);
+    }else{
+        return total_working_ms;
+    }
+}
+
+////////////
+// Changes milliseconds into time in hh:mm:ss:mmmm
+// include_ms changins if milliseconds are added or not.
+///////////
+String ms_to_time(long in_ms, boolean include_ms){
+    String return_str = "";
+    String preceding_string = "";
+
+    if(in_ms < 0){
+        preceding_string = "-";
+        in_ms = abs(in_ms);
+    }
+    //long tmp_seconds_remaining = in_ms/1000;
+    long tmp_ms_remaining = in_ms;///1000;
+    int hours = tmp_ms_remaining/3600000;
+
+    tmp_ms_remaining = tmp_ms_remaining%3600000;
+
+    int minutes = tmp_ms_remaining/60000;
+    tmp_ms_remaining = tmp_ms_remaining%60000;
+
+    int seconds = tmp_ms_remaining/1000;
+    int ms = tmp_ms_remaining%1000;
+
+    char buffer [50];
+    if(include_ms){
+        sprintf (buffer, "%s%01d:%02d:%02d:%03d", preceding_string, hours, minutes, seconds, ms);
+    }else{
+        sprintf (buffer, "%s%01d:%02d:%02d", preceding_string, hours, minutes, seconds);
+    }
+
+    return_str = String(buffer);
+    return return_str;
 }
 
 
